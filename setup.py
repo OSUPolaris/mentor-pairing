e896980c2ce5cbed2772695fb2c12eec45b03833
Liam Dubay <liam.dubay@gmail.com>
2024-08-13 13:49:56 -0400

    add package setup tools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+requirements = [
+    "pandas",
+    "numpy"
+]
+
+requirements_dev = []
+
+setup(
+    name="stablepairing",
+    version="0.1.0",
+    description="Stable pairing algorithm for mentoring programs",
+    url="https://github.com/OSUPolaris/mentor-pairing",
+    author="OSU Polaris Program",
+    packages=["stablepairing"],
+    package_dir={"": "src"},
+    install_requirements=requirements,
+    extras_require={
+        "dev": requirements_dev
+    }
+)
